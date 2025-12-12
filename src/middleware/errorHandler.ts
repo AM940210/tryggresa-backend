8b3238550a6d87aed84b6d06af40d24ddf25d801
Ahmad Mohammadi <ahmmoh0210@gmail.com>
2025-12-12 20:42:37 +0100

    added middleware, errorhandler

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import { logger } from "../lib/logger";
+
+export function errorHandler(
+    err: any,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+) {
+    logger.error("Error", err);
+    res.status(500).json({ error: "Internal server error"});
+}
