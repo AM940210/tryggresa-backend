9e5c59f0b1928280f3c23bd2cf9883f0da8c08a0
Ahmad Mohammadi <ahmmoh0210@gmail.com>
2025-12-12 20:38:08 +0100

    added some routes

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,1 +1,19 @@
 // Error handler & validation middleware.
+import { ZodSchema } from "zod";
+import { Request, Response, NextFunction } from "express";
+
+export const validate =
+    (schema: ZodSchema) =>
+    (req: Request, res: Response, next: NextFunction) => {
+        const parsed = schema.safeParse(req.body);
+
+        if (!parsed.success) {
+            return res.status(400).json({
+                error: "Validation failed",
+                details: parsed.error.errors,
+            });
+        }
+
+        req.body = parsed.data;
+        next();
+    };
--- src/validators/trips.schema.ts
+++ src/validators/trips.schema.ts
@@ -1,1 +1,13 @@
 // Zod schema för att validera inkommande request-body.
+import { z } from "zod";
+
+export const tripSchema = z.object({
+    date: z.string(),
+    time: z.string(),
+    fromAddress: z.string().optional(),
+    toAddress: z.string().optional(),
+    people: z.number().min(1),
+    wheelchair: z.boolean()
+});
+
+export type TrypInput = z.infer<typeof tripSchema>;
